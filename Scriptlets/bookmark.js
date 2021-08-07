48814d01fe51a4ff3ab3536f12d70cdc0fb99536
John Sanchez <github@sanchez.llc>
2021-08-06 20:10:37 -0500

    bookmark.js

--- Scriptlets/bookmark.js
+++ Scriptlets/bookmark.js
@@ -1,5 +1,8 @@
 Clear DB Cache
-javascript:var r = new XMLHttpRequest();  r.open("GET", "https://raw.githubusercontent.com/albinobean/MCX-Bookmarklets/master/Clear DB Cache.js", true);            r.onloadend = function (oEvent) {      new Function(r.responseText)();  };  r.send();
+javascript: var r = new XMLHttpRequest(); r.open("GET", "https://raw.githubusercontent.com/albinobean/MCX-Bookmarklets/master/Clear DB Cache.js", true); r.onloadend = function (oEvent) { new Function(r.responseText)(); }; r.send();
 
 Share DB
-javascript:var r = new XMLHttpRequest();  r.open("GET", "https://raw.githubusercontent.com/albinobean/MCX-Bookmarklets/master/Share Dashboard.js", true);            r.onloadend = function (oEvent) {      new Function(r.responseText)();  };  r.send();
+javascript: var r = new XMLHttpRequest(); r.open("GET", "https://raw.githubusercontent.com/albinobean/MCX-Bookmarklets/master/Share Dashboard.js", true); r.onloadend = function (oEvent) { new Function(r.responseText)(); }; r.send();
+
+Unique Export values
+javascript: var r = new XMLHttpRequest(); r.open("GET", "https://raw.githubusercontent.com/albinobean/IM-Extensibility/master/Scriptlets/Unique%20export%20fields.js", true); r.onloadend = function (oEvent) { new Function(r.responseText)(); }; r.send();
